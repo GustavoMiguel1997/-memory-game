715a88991cb1034d67b434f34d84ba704ab22a8c
GustavoMiguel1997 <42610791+GustavoMiguel1997@users.noreply.github.com>
2022-11-21 19:06:29 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.use('/', express.static(path.resolve(__dirname, 'dist')));
 
 app.listen(process.env.PORT || 5000);
